5b69a1cc4043cf83a2a4879704fe5ccf8334e8e1
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-05-26 16:09:35 -0300

    Back to pytest. Fix comment

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -1,18 +1,16 @@
-import unittest
+import pytest
 from unittest import mock
 import os
 from colonel.wrapper.wrapper import Wrapper
 from colonel.wrapper.errors import SimulatorExecutableNotFound
 
-class WrapperTests(unittest.TestCase):
-
-    def test_no_simulator_executable_found(self):
-        with self.assertRaises(SimulatorExecutableNotFound):
-            with mock.patch.dict(os.environ, {'PATH': ''}):
-                wrapper = Wrapper()
-
-    def test_simulator_executable_found_in_PATH(self):
-        # Set current working directoy in path, so it discovers 'test_cd++'
-        test_path_directory = os.path.dirname(os.path.abspath(__file__))
-        with mock.patch.dict(os.environ, {'PATH': test_path_directory}):
+def test_no_simulator_executable_found():
+    with pytest.raises(SimulatorExecutableNotFound):
+        with mock.patch.dict(os.environ, {'PATH': ''}):
             wrapper = Wrapper()
+
+def test_simulator_executable_found_in_PATH():
+    # Set current working directory in path, so it discovers 'cd++' in tests/
+    test_path_directory = os.path.dirname(os.path.abspath(__file__))
+    with mock.patch.dict(os.environ, {'PATH': test_path_directory}):
+        wrapper = Wrapper()
