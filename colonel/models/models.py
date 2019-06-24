761db5c6628ae4323e24c1b3d15ac05fc5fca89a
Juan Lanuza <juan.lanuza3@gmail.com>
2019-06-24 02:58:50 -0300

    Initial modeling classes

---
+++ colonel/models/models.py
@@ -0,0 +1,59 @@
+from typing import Sequence
+
+
+class Port:
+    def __init__(self, name: str):
+        # a type could be added in the future as an extra checks
+        self.name = name
+
+
+class InPort(Port):
+    pass
+
+
+class OutPort(Port):
+    pass
+
+
+class Link:
+    def __init__(self, from_port: Port, to_port: Port):
+        self.from_port = from_port
+        self.to_port = to_port
+
+
+class ExtInputLink(Link):
+    def __init__(self, from_port: InPort, to_port: OutPort):
+        super().__init__(from_port, to_port)
+
+
+class ExtOutputLink(Link):
+    def __init__(self, from_port: OutPort, to_port: OutPort):
+        super().__init__(from_port, to_port)
+
+
+class IntLink(Link):
+    def __init__(self, from_port: OutPort, to_port: InPort):
+        super().__init__(from_port, to_port)
+
+
+class Model:
+    pass
+
+
+class Atomic(Model):
+    def __init__(self, inports: Sequence[InPort], outports: Sequence[OutPort]):
+        self.inports = inports
+        self.outports = outports
+
+
+class Coupled(Model):
+    def __init__(self, subcomponents: Model,
+                 inports: Sequence[InPort], outports: Sequence[OutPort],
+                 eic: Sequence[ExtInputLink], eoc: Sequence[ExtOutputLink],
+                 ic: Sequence[IntLink]):
+        self.subcomponents = subcomponents
+        self.inports = inports
+        self.outports = outports
+        self.eic = eic
+        self.eoc = eoc
+        self.ic = ic
