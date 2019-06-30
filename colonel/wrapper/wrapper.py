ad0d06e06dbcb50a6f20cc58b26eb0996fb90382
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-06-30 13:17:39 -0300

    Tests and wrapper adapted

--- colonel/wrapper/wrapper.py
+++ colonel/wrapper/wrapper.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 from colonel.wrapper.errors import SimulatorExecutableNotFound
 
+from colonel.wrapper.config import CDPP_BIN_PATH
+
 
 class ExecutableDiscoverer():
     def discover(self) -> Optional[str]:
@@ -58,7 +60,7 @@ class LibraryEnvironmentVarExecutableDiscoverer(ExecutableDiscoverer):
 
 class LibraryDefinedDirectoryExecutableDiscoverer(ExecutableDiscoverer):
     def do_discover(self):
-        executable_route = os.path.join(Wrapper.CDPP_LIBRARY_DEFINED_EXECUTABLE_PATH,
+        executable_route = os.path.join(CDPP_BIN_PATH,
                                         Wrapper.CDPP_BIN)
         if ExecutableDiscoverer.is_executable_file(executable_route):
             return executable_route
@@ -78,8 +80,6 @@ class Wrapper:
 
     def discover_executable_route(self):
         discoverer = CompoundExecutableDiscoverer(
-            PathEnvironmentVarExecutableDiscoverer(),
-            LibraryEnvironmentVarExecutableDiscoverer(),
             LibraryDefinedDirectoryExecutableDiscoverer(),
         )
         found_route = discoverer.discover()
