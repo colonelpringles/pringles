50c8f7e07973f2dc103eb7603d1cb97143b40cad
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-21 04:13:26 -0300

    Remove completely unnecessary discoverers.

--- colonel/wrapper/wrapper.py
+++ colonel/wrapper/wrapper.py
@@ -1,87 +1,21 @@
 import os
 
-from typing import Optional
-
 from colonel.wrapper.errors import SimulatorExecutableNotFound
 
 from colonel.wrapper.config import CDPP_BIN_PATH
 
 
-class ExecutableDiscoverer():
-    def discover(self) -> Optional[str]:
-        return self.do_discover()
-
-    # Discoverer implementation
-    def do_discover(self) -> Optional[str]:
-        raise NotImplementedError()
-
-    @staticmethod
-    def is_executable_file(filepath: str) -> bool:
-        return os.path.isfile(filepath) and os.access(filepath, os.X_OK)
-
-
-class CompoundExecutableDiscoverer(ExecutableDiscoverer):
-    def __init__(self, *discoverers: ExecutableDiscoverer):
-        self.discoverers = discoverers
-
-    def do_discover(self) -> Optional[str]:
-        found_route = None
-        for discoverer in self.discoverers:
-            found_route = discoverer.discover()
-            if found_route:
-                return found_route
-        return None
-
-
-class PathEnvironmentVarExecutableDiscoverer(ExecutableDiscoverer):
-    PATH_ENV_VAR = "PATH"
-
-    def do_discover(self) -> Optional[str]:
-        path = os.environ.get(PathEnvironmentVarExecutableDiscoverer.PATH_ENV_VAR, '')
-        for directory in path.split(os.pathsep):
-            executable_route = os.path.join(directory, Wrapper.CDPP_BIN)
-            if ExecutableDiscoverer.is_executable_file(executable_route):
-                return executable_route
-        return None
-
-
-class LibraryEnvironmentVarExecutableDiscoverer(ExecutableDiscoverer):
-    def do_discover(self):
-        try:
-            cdpp_bin_directory = os.environ.get(Wrapper.CDPP_EXECUTABLE_ENV_VAR)
-            if cdpp_bin_directory is not None:
-                executable_route = os.path.join(cdpp_bin_directory, Wrapper.CDPP_BIN)
-                if ExecutableDiscoverer.is_executable_file(executable_route):
-                    return executable_route
-        except KeyError:
-            pass
-        return None
-
-
-class LibraryDefinedDirectoryExecutableDiscoverer(ExecutableDiscoverer):
-    def do_discover(self):
-        executable_route = os.path.join(CDPP_BIN_PATH,
-                                        Wrapper.CDPP_BIN)
-        if ExecutableDiscoverer.is_executable_file(executable_route):
-            return executable_route
-        return None
-
-
 class Wrapper:
     CDPP_BIN = 'cd++'
-    CDPP_EXECUTABLE_ENV_VAR = 'CDPP_BIN'
-
-    DRAWLOG_BIN = 'drawlog'
-    simulationAbortedErrorMessage = 'Aborting simulation...\n'
 
     def __init__(self):
         self.executable_route = self.discover_executable_route()
 
-    def discover_executable_route(self):
-        discoverer = CompoundExecutableDiscoverer(
-            LibraryDefinedDirectoryExecutableDiscoverer(),
-        )
-        found_route = discoverer.discover()
-        if found_route is None:
+    def discover_executable_route(self) -> str:
+        filepath = os.path.join(CDPP_BIN_PATH, self.CDPP_BIN)
+        is_simulator_executable_present = os.path.isfile(filepath) \
+            and os.access(filepath, os.X_OK)
+
+        if is_simulator_executable_present is None:
             raise SimulatorExecutableNotFound()
-        return found_route
+        return filepath
--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -11,28 +11,6 @@ TEST_EXECUTABLE = os.path.join(TEST_PATH_DIRECTORY, Wrapper.CDPP_BIN)
 CDPP_BIN_EXECUTABLE_PATH = os.path.join(CDPP_BIN_PATH, Wrapper.CDPP_BIN)
 
 
-@pytest.mark.skip(reason = "Deprecated executable locator")
-@mock.patch.dict(os.environ, {'PATH': ''})
-def test_no_simulator_executable_found():
-    with pytest.raises(SimulatorExecutableNotFound):
-        wrapper = Wrapper()  # noqa: F841
-
-
-@pytest.mark.skip(reason = "Deprecated executable locator")
-@mock.patch.dict(os.environ, {'PATH': TEST_PATH_DIRECTORY})
-def test_simulator_executable_found_in_PATH():
-    # Set current working directory in path, so it discovers 'cd++' in tests/
-    wrapper = Wrapper()
-    assert wrapper.executable_route == TEST_EXECUTABLE
-
-
-@pytest.mark.skip(reason = "Deprecated executable locator")
-@mock.patch.dict(os.environ, {'PATH': '', Wrapper.CDPP_EXECUTABLE_ENV_VAR: TEST_PATH_DIRECTORY})
-def test_simulator_executable_found_in_library_env_var():
-    wrapper = Wrapper()
-    assert wrapper.executable_route == TEST_EXECUTABLE
-
-
 def test_simulator_executable_found_in_library_defined_dir():
     wrapper = Wrapper()
     assert wrapper.executable_route == CDPP_BIN_EXECUTABLE_PATH
