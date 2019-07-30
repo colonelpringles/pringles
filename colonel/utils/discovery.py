71218da953318b099d45864456efb0e4c9ec7bb7
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-30 01:38:30 -0300

    Test added. Failing.

---
+++ colonel/utils/discovery.py
@@ -0,0 +1,16 @@
+from typing import Union, List
+from colonel.models import InPort, OutPort, Port
+from io import StringIO  # File typing
+
+
+class AtomicMetadataExtractor:
+    """Extractor from ports metadata from Atomcis cpp file."""
+    def __init__(self, source: Union[str, StringIO]):
+        if isinstance(source, str):
+            self.source = source
+        else:
+            self.source = source.read()
+
+    def extract_ports(self) -> List[Port]:
+        return []
+    
---
+++ tests/test_atomic_metadata_extractor.py
@@ -0,0 +1,12 @@
+import pytest
+from colonel.utils.discovery import AtomicMetadataExtractor
+from colonel.models import InPort, OutPort
+
+
+def test_single_input_port_extracted_correctly():
+    source = "@InputPort(\"in\")"
+    assert AtomicMetadataExtractor(source).extract_ports() == [InPort("in", None)]
+
+def test_single_output_port_extracted_correctly():
+    source = "@OutputPort(\"out\")"
+    assert AtomicMetadataExtractor(source).extract_ports() == [OutPort("out", None)]
