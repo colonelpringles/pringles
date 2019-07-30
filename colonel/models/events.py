fc761589d1beea1f6768fc87cd0a544bd73f6233
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-30 00:38:52 -0300

    Documentation. Unecessary whitespace removal

--- colonel/models/events.py
+++ colonel/models/events.py
@@ -4,14 +4,14 @@ from typing import Union, List
 
 
 class Event:
-    """
-    Event is the representation of external influence into the model.
-    And event is composed of:
-        - event time
-        - target port, belonging to the top model
-        - value
-    """
+    """Event is the representation of external influence into the model.
+    An event is composed of:
 
+    * event time
+    * target port, belonging to the top model
+    * value
+
+    """
     def __init__(self,
                  time: VirtualTime,
                  port: Port,
@@ -21,6 +21,5 @@ class Event:
         self.value = value
 
     def serialize(self) -> str:
-        # Remove all whitesapce in value str representation
-        value_as_str = str(self.value).replace(" ", "")
-        return f"{str(self.time)} {self.target_port.name} {value_as_str};"
+        """Serialize the :class:`Event` into a CD++ consumable string."""
+        return f"{str(self.time)} {self.target_port.name} {str(self.value)};"
--- tests/test_events.py
+++ tests/test_events.py
@@ -9,7 +9,7 @@ one_hour_time = VirtualTime.of_hours(1)
 @pytest.mark.parametrize("event,expected_serialization", [
     (Event(one_hour_time, sample_port, 1.5), "1:0:0:0 sample_port 1.5;"),
     (Event(one_hour_time, sample_port, 1), "1:0:0:0 sample_port 1;"),
-    (Event(one_hour_time, sample_port, [1.5, 2.3]), "1:0:0:0 sample_port [1.5,2.3];"),
+    (Event(one_hour_time, sample_port, [1.5, 2.3]), "1:0:0:0 sample_port [1.5, 2.3];"),
 ])
 def test_event_serialized_correctly(event: Event, expected_serialization: str):
     assert event.serialize() == expected_serialization
