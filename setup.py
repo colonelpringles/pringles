d0045cadec594f42dbba1bf5655dd2a1e7251d56
Juan Lanuza <juan.lanuza3@gmail.com>
2019-08-21 01:37:26 -0300

    adding install_requires param in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+with open('requirements.txt', 'r') as f:
+    dependencies = [line.strip() for line in f.readlines()]
+
 setuptools.setup(
     name="pringles-devs",
     version="0.0.2",
@@ -18,4 +22,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
+    install_requires=dependencies
 )
