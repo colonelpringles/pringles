adf6e25d0dcf4641f730ab7cd4dfe4728a9658ef
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-08-25 19:21:37 -0300

    Added new vtime aware axes

--- pringles/utils/__init__.py
+++ pringles/utils/__init__.py
@@ -1,3 +1,4 @@
 from .utils import VirtualTime  # noqa: F401
 from .discovery import AtomicMetadataExtractor, AtomicMetadata  # noqa: F401
 from .errors import MetadataParsingException  # noqa: F401
+from .plotting import new_vtime_aware_axis  # noqa: F401
