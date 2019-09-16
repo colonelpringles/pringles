03c6f1f40291a42c5bc9861ae5992507c6552f4f
PringlesDeploymentBot (from Travis CI) <deploy@travis-ci.org>
2019-09-16 03:36:14 +0000

    Deploy colonelpringles/pringles to github.com/colonelpringles/pringles.git:gh-pages

--- tests/test_model_metadata_and_instanciation_integration.py
+++ tests/test_model_metadata_and_instanciation_integration.py
@@ -1,20 +1,39 @@
 import pytest  # noqa
 import os
 from pringles.models import InPort, OutPort
+from pringles.utils.errors import NonExistingAtomicClassException
 from pringles.simulator import Simulator
 
 
 CDPP_BIN_PATH = os.path.join(os.path.dirname(__file__), '../cdpp/src/bin/')
 
 
-def test_extract_metadata_and_instantiate_is_coherent():
+@pytest.fixture
+def simulator_with_rocket_on_registry():
     simulator = Simulator(CDPP_BIN_PATH, "tests/resources/")
-    assert simulator.get_registry().Rocket is not None
+    yield simulator
+    import pringles.models.models as pringles_models_module
+    del pringles_models_module.Rocket
 
-    awesome_queue = simulator.get_registry().Rocket("awesome")
+
+def test_extract_metadata_and_instantiate_is_coherent(simulator_with_rocket_on_registry):
+    assert simulator_with_rocket_on_registry.get_registry().Rocket is not None
+
+    awesome_queue = simulator_with_rocket_on_registry.get_registry().Rocket("awesome")
     assert awesome_queue.get_port("in") is not None
     assert awesome_queue.get_port("done") is not None
     assert awesome_queue.get_port("out") is not None
     assert isinstance(awesome_queue.get_port("in"), InPort)
     assert isinstance(awesome_queue.get_port("done"), InPort)
     assert isinstance(awesome_queue.get_port("out"), OutPort)
+
+
+def test_registry_get_by_name_method(simulator_with_rocket_on_registry):
+    registry = simulator_with_rocket_on_registry.get_registry()
+    assert registry.Rocket == registry.get_by_name("Rocket")
+
+
+def test_registry_get_by_name_non_existing_atomic(simulator_with_rocket_on_registry):
+    registry = simulator_with_rocket_on_registry.get_registry()
+    with pytest.raises(NonExistingAtomicClassException):
+        registry.get_by_name("Falopa")
